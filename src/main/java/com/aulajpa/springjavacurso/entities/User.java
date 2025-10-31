60b51d5439ea7e7370b90c7cca28a750b3656988
andrelsrn <andrel.srn@hotmail.com>
2025-10-31 09:52:43 -0300

    H2 database, test profile, JPA

--- src/main/java/com/aulajpa/springjavacurso/entities/User.java
+++ src/main/java/com/aulajpa/springjavacurso/entities/User.java
@@ -1,15 +1,22 @@
 package com.aulajpa.springjavacurso.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
+
 
     public User() {
     }
