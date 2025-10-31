a02724f72664e82c1f3f98fa935501277eea4e14
andrelsrn <andrel.srn@hotmail.com>
2025-10-31 09:01:19 -0300

    Project Create

---
+++ src/main/java/com/aulajpa/springjavacurso/SpringJavaCursoApplication.java
@@ -0,0 +1,13 @@
+package com.aulajpa.springjavacurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaCursoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJavaCursoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aulajpa/springjavacurso/SpringJavaCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulajpa.springjavacurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaCursoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
