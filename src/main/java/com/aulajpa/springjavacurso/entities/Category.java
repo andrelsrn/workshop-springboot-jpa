663795a256c6d308dd2127d9b030b8558659adc4
andrelsrn <andrel.srn@hotmail.com>
2025-11-04 13:41:56 -0300

    Product

--- src/main/java/com/aulajpa/springjavacurso/entities/Category.java
+++ src/main/java/com/aulajpa/springjavacurso/entities/Category.java
@@ -3,16 +3,22 @@ package com.aulajpa.springjavacurso.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -38,6 +44,10 @@ public class Category implements Serializable {
         this.id = id;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -49,4 +59,6 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
 }
